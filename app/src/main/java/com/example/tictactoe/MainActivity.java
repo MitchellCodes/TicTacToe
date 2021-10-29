167595dcf4ed2e6ec50f9012436131340be0e386
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-10-28 21:42:06 -0700

    Assignment#2 (#1)
    * Updated Gradle plugin
    
    * Put needed strings into strings.xml
    
    * Made the buttons be marked with the symbol of the player who clicked it
    
    * Auto generated file
    
    * Made the current player change when a tile is marked
    
    * Linked New Game button to clearBoard() method
    
    * Made clearBoard() re-enable and clear the buttons
    
    * Changes the current player to X's when clearBoard() is called

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // for the future when the game needs to determine a winner.
+    // idea: have startNewGame() contain a loop that
+    // calls other methods so players keep taking turns
+    // picking tiles until someone wins or there are
+    // no tiles left to pick
+//    public void startNewGame() {
+//        String[][] emptyBoard = {
+//                {"", "", ""},
+//                {"", "", ""},
+//                {"", "", ""}
+//        };
+//    }
+
+    /**
+     * Gets all of the buttons that are used in the game
+     * and clears their text as well as re-enables them.
+     * Also sets the current player's turn to X's.
+     */
+    public void clearBoard(View view) {
+        Button[] allTileButtons = {
+                findViewById(R.id.topLeftButton),
+                findViewById(R.id.topCenterButton),
+                findViewById(R.id.topRightButton),
+                findViewById(R.id.centerLeftButton),
+                findViewById(R.id.centerButton),
+                findViewById(R.id.centerRightButton),
+                findViewById(R.id.bottomLeftButton),
+                findViewById(R.id.bottomCenterButton),
+                findViewById(R.id.bottomRightButton)
+        };
+
+        for (Button currentButton : allTileButtons) {
+            currentButton.setText(""); // clear button out
+            currentButton.setEnabled(true); // re-enable the button
+        }
+
+        TextView outputText = findViewById(R.id.outputText);
+        outputText.setText(R.string.playerXTurn);
+    }
+
+    /**
+     * Determines what player's turn it is by checking the
+     * text in the View with the id outputText.
+     * @return The symbol that is taking their turn as a string
+     */
+    public String getCurrentPlayerSymbol() {
+        TextView outputText = findViewById(R.id.outputText);
+        // looks in strings.xml for string with name of playerXTurn
+        if (outputText.getText().equals("Player X's turn")) {
+            outputText.setText(R.string.playerOTurn);
+            return "X";
+        }
+        else {
+            outputText.setText(R.string.playerXTurn);
+            return "O";
+        }
+    }
+
+    /**
+     * Marks the button that was clicked with the
+     * current player's symbol.
+     * @param view The button that was clicked
+     */
+    public void markTile(View view) {
+        String currentPlayer = getCurrentPlayerSymbol();
+        Button button = (Button) view;
+        button.setText(currentPlayer);
+        view.setEnabled(false);
     }
 }
